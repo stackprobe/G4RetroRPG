b9cefa2e392f926ede571199375002ec826b1486
stackprobe <stackprobes@gmail.com>
2019-08-22 15:43:23 +0900

    Flower

--- RedDiamond/_copylib.bat
+++ RedDiamond/_copylib.bat
@@ -1,2 +1,3 @@
 C:\Factory\SubTools\CopyLib.exe C:\Dev\Fairy\Donut2\Donut2\Donut2\Common RedDiamond\Common
+C:\Factory\SubTools\CopyLib.exe C:\Dev\CSharp\Chocolate\Chocolate\Tools RedDiamond\Tools
 ROBOCOPY C:\Dev\Fairy\Donut2\Resource\General ..\Resource\General /MIR
--- _Release.bat
+++ _Release.bat
@@ -2,8 +2,8 @@ C:\Factory\Tools\RDMD.exe /RC out
 
 C:\Factory\SubTools\makeDDResourceFile.exe Resource out\Resource.dat
 
-COPY /B RedDiamond\RedDiamond\bin\Release\RedDiamond.exe out
-COPY /B RedDiamond\RedDiamond\bin\Release\Chocolate.dll out
+C:\Factory\SubTools\CallConfuserCLI.exe RedDiamond\RedDiamond\bin\Release\RedDiamond.exe out\RedDiamond.exe
+rem COPY /B RedDiamond\RedDiamond\bin\Release\RedDiamond.exe out
 COPY /B RedDiamond\RedDiamond\bin\Release\DxLib.dll out
 COPY /B RedDiamond\RedDiamond\bin\Release\DxLib_x64.dll out
 COPY /B RedDiamond\RedDiamond\bin\Release\DxLibDotNet.dll out
