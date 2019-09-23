4cf4206fbc500ab634058da035aaa29ad9400047
stackprobe <stackprobes@gmail.com>
2019-09-24 02:44:51 +0900

    Pine

--- RedDiamond/_copylib.bat
+++ RedDiamond/_copylib.bat
@@ -1,2 +1,3 @@
 C:\Factory\SubTools\CopyLib.exe C:\Dev\Fairy\Donut2\Donut2\Donut2\Common RedDiamond\Common
+C:\Factory\SubTools\CsProjAdjustCs.exe RedDiamond Common
 ROBOCOPY C:\Dev\Fairy\Donut2\Resource\General ..\Resource\General /MIR
