1d64f7d1974bc529452f5eb16f9d15827f2190cd
stackprobe <stackprobes@gmail.com>
2020-08-12 06:24:46 +0900

    Southern Cross

--- Release.bat
+++ Release.bat
@@ -1,6 +1,6 @@
 C:\Factory\Tools\RDMD.exe /RC out
 
-C:\Factory\SubTools\makeDDResourceFile.exe Resource out\Resource.dat Tools\MaskGZData.exe
+C:\Factory\SubTools\makeDDResourceFile.exe C:\Dat\Resource out\Resource.dat C:\Factory\Program\MaskGZDataForDonut3\MaskGZData.exe
 
 C:\Factory\SubTools\CallConfuserCLI.exe RedDiamond\RedDiamond\bin\Release\RedDiamond.exe out\RedDiamond.exe
 rem COPY /B RedDiamond\RedDiamond\bin\Release\RedDiamond.exe out
@@ -10,7 +10,7 @@ COPY /B RedDiamond\RedDiamond\bin\Release\DxLib_x64.dll out
 COPY /B RedDiamond\RedDiamond\bin\Release\DxLibDotNet.dll out
 
 C:\Factory\Tools\xcp.exe doc out
-C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
+C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut3\doc out
 
 C:\Factory\SubTools\zip.exe /PE- /RVE- /G out RedDiamond
 C:\Factory\Tools\summd5.exe /M out
