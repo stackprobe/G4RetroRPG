469dc30bd164c9f36976ebd41155f7ccfb7c4091
stackprobe <stackprobes@gmail.com>
2020-08-12 07:43:53 +0900

    Melody

--- RedDiamond/RedDiamond/Program.cs
+++ RedDiamond/RedDiamond/Program.cs
@@ -17,7 +17,7 @@ namespace Charlotte
 			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
 		}
 
-		public const string APP_IDENT = "{89f60f15-0bd3-4a0a-a322-0eee3b8393de}";
+		public const string APP_IDENT = "{c69c45fc-c710-40ed-ad6d-d905db64d907}";
 		public const string APP_TITLE = "RedDiamond";
 	}
 }
